5f4a9d1620e259f13492d02676c70c710f34e225
raph <raphael.mottelet@protonmail.com>
2023-04-06 16:42:33 +0200

    display tags

--- django_taggit/core/admin.py
+++ django_taggit/core/admin.py
@@ -3,4 +3,11 @@ from core.models import Items
 
 # Register your models here.
 
-admin.site.register(Items)
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'get_tags']
+
+    def get_tags(self, obj):
+        return ",".join(tag.name for tag in obj.tags.all())
+
+    get_tags.short_description = 'Tags'
